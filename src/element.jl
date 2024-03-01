7d71c3be769acf1eebf668d7609884c81c8ab16b
cont00rs <cont00rs@users.noreply.github.com>
2024-03-01 16:43:32 +0100

    Evaluate shape functions at Element construction
    The patch replaces the hardcoded shape function values for the one
    dimensional element with shape functions that are generated as the
    cartesian product of the one-dimensional element.
    
    The Element struct is updated to reflect that N is expected to be any
    AbstractMatrix. In practice, this is a SMatrix, given that the size and
    values are known at compile time and will not change throughout.

--- src/element.jl
+++ src/element.jl
@@ -1,10 +1,11 @@
 using LinearAlgebra
+using StaticArrays
 
 export Element, element_matrix
 
 struct Element
     # Shape function
-    N::Any
+    N::AbstractMatrix
     # Shape function derivatives
     B::Any
     # Jacobian
@@ -20,10 +21,20 @@ shape_dfn(element) = element.B
 measure(element::Element) = det(element.J)
 stencil(element) = element.K
 
+shape_function(x::Real) = [1.0 - x, x]
+
+# XXX: The shape functions definition should be aligned with the integration
+# domains [-1, +1]. These are now still defined as [[0, 1], which is OK, until
+# the quadrature is introduced too.
+function shape_function(dim::Integer)
+    # XXX: For now hardcoded evaluated at element center (0.5).
+    Base.product(repeat([shape_function(0.5)], dim)...) .|> prod |> SMatrix{1,2^dim}
+end
+
 # TODO: Convert to proper struct constructor?
-function element_matrix(mesh)
+function element_matrix(mesh::Mesh{Dim}) where {Dim}
     # Shape fun
-    N = [0.5 0.5]
+    N = shape_function(Dim)
     # Shape fun derivative
     b = [-1, 1]
     J = b' * measure(mesh)
