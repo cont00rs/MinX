99e667b56ec1388a2f038d40cd68647ca7f4747c
cont00rs <cont00rs@users.noreply.github.com>
2024-04-20 12:09:03 +0200

    Use StaticArrays to include dof size in type
    The mutable static array can be used to communicate the array's
    dimension at compile time and avoid repeated calls to figure out the
    array sizes.

--- src/main.jl
+++ src/main.jl
@@ -3,30 +3,23 @@ using LinearAlgebra
 export fix!, solve, prescribe
 
 # XXX: Probably want to store `LinearIndices` instance somewhere to prevent reallocs.
-#
 dof(mesh, node, dpn) = LinearIndices((dpn, Tuple(mesh.nelems .+ 1)...))[node]
 
 function dofs!(
-    array::AbstractMatrix{T},
+    array::MMatrix{Dpn, N, T},
     mesh::Mesh{Dim},
     nodes::AbstractVector{CartesianIndex{Dim}},
-    dofs_per_node,
-) where {T,Dim}
+) where {Dpn,N,T,Dim,}
     # For scalar field data is aligned as: Matrix(nx, ny)
     # For vector field data is aligned as: Matirx(2, nx, ny)
     # The node remains the same cartesian index, (ni, nj)
+    #
     # Which is then transformed to index different components of the vector:
     #   x: CartesianIndex(1, ci) with ci the CartesianIndex of (ni, nj)
     #   y: CartesianIndex(2, ci) with ci the CartesianIndex of (ni, nj)
-    #
-    # Would it be sufficient to add a second loop here over number of dofs per node?
-    #
-    # The `LinearIndices` accessing done in `dof` should then be expeded too.
-    # That information should live somewhere separate, in a separate type that
-    # dispatch can work on.
-    for d = 1:dofs_per_node
+    for d in axes(array, 1)
         for (i, node) in enumerate(nodes)
-            array[d, i] = dof(mesh, CartesianIndex(d, node), dofs_per_node)
+            array[d, i] = dof(mesh, CartesianIndex(d, node), Dpn)
         end
     end
 end
